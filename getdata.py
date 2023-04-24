872dc2ba1a712a55c3ec72dbdf831bc02ad12bb2
Felix Stenberg <felixste@kth.se>
2023-04-24 11:44:02 +0200

    Added a file that pulls data from yFinance

---
+++ getdata.py
@@ -0,0 +1,14 @@
+import yfinance as yf
+
+def pull_data_realtime(stock_symbol):
+    ticker = yf.Ticker(stock_symbol)
+    
+    # Get historical data
+    data = ticker.history(period="1d", interval="1m")
+    
+    # Alternatively, you can access various stock information
+    info = ticker.info
+    
+    return data, info
+
+print(pull_data_realtime("AAPL"))
