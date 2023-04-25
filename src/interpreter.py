1ded71c92cfb929ef3b4b64a77af0bf70deaa09e
Axel Axhag <axel@Lenovo_E15.localdomain>
2023-04-25 18:20:03 +0200

    python file for interpreting commands such as buy, sell, portfolio and logs

---
+++ src/interpreter.py
@@ -0,0 +1,27 @@
+# Interpreter for interpreting commands through the command line console 
+# buy AAPL 10(amount) 200(price per stock)
+from user import *
+
+# Switch case for deciding which commands to execute
+def commandRead(command):
+    words = command.split()
+    if (len(words) == 3):
+        words.append(0)                    # Sets the amount to 0 which says that the stock is listed for the current selling price 
+    if (int(words[2]) < 1):                      
+        print("Cannot buy < 1 stocks!")     # if you set a negative or 0 amount of stocks then give an error
+        return
+    if (int(words[3]) < 0): 
+        print("Cannot sell stocks for < 0!")     # You can't buy stocks for negative money
+        return
+    match words[0]:
+        case "buy":
+            username.buy_Stocks(int(words[2]), int(words[3]), words[1])
+        case "sell":
+            username.sell_Stocks(int(words[2]), int(words[3]), words[1])
+        case "portfolio":
+            print(username.stocks)
+        case "logs":
+            #TODO add logs to print
+            print()
+        case defualt:
+            print(f"The command '{words[0]}' does not exist")
