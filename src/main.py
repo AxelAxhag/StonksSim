7db00d697d4c7d15c7ec8c946ad94b51dd8a23af
Axel Axhag <axel@Lenovo_E15.localdomain>
2023-04-26 17:57:42 +0200

    User saves

--- src/main.py
+++ src/main.py
@@ -2,11 +2,24 @@
 # The python file where the program runs
 from interpreter import *
 from user import *
+from save import *
 
+nameInput = input("What is your username?: ").strip()
+activeUser = user(nameInput, 0)
 
+if (userFileExists(activeUser)):
+    loadUserData(activeUser)
+    print(f"Welcome back, {activeUser.username}!")
+    print(f"Your balance is ${activeUser.balance}.\n")
+    
+else:
+    createUserFile(activeUser)
+    balanceInput = input("You're a new user! \n Input a balance to start with: ")
+    activeUser.balance = balanceInput
 
 
 while(True):
     command = input()
     commandRead(command)
+    writeUserData(activeUser)
 
