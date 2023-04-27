00bf9d2315456ccc934edef456bf72097a02fcb3
Axel Axhag <axel@Lenovo_E15.localdomain>
2023-04-27 15:52:35 +0200

    Small string output fix where there was unnecessary whitspacee

--- src/main.py
+++ src/main.py
@@ -15,8 +15,8 @@ if (userFileExists(activeUser)):
     
 else:
     createUserFile(activeUser)
-    balanceInput = input("You're a new user! \n Input a balance to start with: ")
-    activeUser.balance = balanceInput
+    balanceInput = input("You're a new user!\nInput a balance to start with: ")
+    activeUser.balance = float(balanceInput)
     writeUserData(activeUser)
 
 infoForUser = "To buy a stock, the command is 'buy AAPL 10 200' where 'AAPL' is the stock symbol, '10' is the amount of stocks to buy, and '200' is the price per stock.\nTo sell a stock, the command is 'sell AAPL 5 250' where 'AAPL' is the stock symbol, '5' is the amount of stocks to sell, and '250' is the price per stock. \nTo view the user's current portfolio, the command is 'portfolio'."
