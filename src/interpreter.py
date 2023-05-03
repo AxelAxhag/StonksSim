7c33a75054ffada19816b5705ef0750b33d864dc
Axel Axhag <axel@LenovoE15>
2023-05-03 18:09:21 +0200

    'Cancel' command for cancelling buy/sell orders also documentation for the command

--- src/interpreter.py
+++ src/interpreter.py
@@ -9,7 +9,7 @@ def commandRead(command, self):
         words.append(0)                    # Sets the amount to 0 which says that the stock is listed for the current selling price 
     if(len(words) >=3 ):
         if (int(words[2]) < 1):                      
-            print("Cannot buy < 1 stocks!")     # if you set a negative or 0 amount of stocks then give an error
+            print("Cannot buy/sell < 1 stocks!")     # if you set a negative or 0 amount of stocks then give an error
             return
         if (float(words[3]) < 0): 
             print("Cannot sell stocks for < 0!")     # You can't buy stocks for negative money
@@ -28,6 +28,8 @@ def commandRead(command, self):
         case "exit":
             os._exit(0)
         case "help":
-            print("[buy] {name of stock} {amount of stocks} {price per stock (optional parameter)}\n[sell] {name of stock} {amount of stocks} {price per stock (optional parameter)}\n[portfolio] -- Lists all of the user's balance and stocks\n[cash] -- Prints the user's balance\n[help] -- Prints this prompt for extra help\n[quit] -- Quits/exits the program\n[exit] -- Quits/exits the program\n\nTip: The last parameter in the buy/sell command syntax is optional. If it is blank then the buy/sell order will be set at market price for each stock!")
+            print("[buy] {name of stock} {amount of stocks} {price per stock (optional parameter)}\n[sell] {name of stock} {amount of stocks} {price per stock (optional parameter)}\n[cancel] {buy/sell} {order number} -- Cancels a sell/buy order. You get the order number by using the 'portfolio' command.\n[portfolio] -- Lists all of the user's balance and stocks\n[cash] -- Prints the user's balance\n[help] -- Prints this prompt for extra help\n[quit] -- Quits/exits the program\n[exit] -- Quits/exits the program\n\nTip: The last parameter in the buy/sell command syntax is optional. If it is blank then the buy/sell order will be set at market price for each stock!")
+        case "cancel":
+            self.remove_order(words[1], int(words[2]))
         case defualt:
             print(f"The command '{words[0]}' does not exist!\nIf you're struggling with the commands then simply input 'help' into the console!")
